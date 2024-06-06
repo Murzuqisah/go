91e0adabecd8c71513d8c724d150d76f1fcde89c
ramuiruri <rayjaymuiruri@gmal.com>
2024-06-06 16:40:17 +0300

    solutions.go

--- solutions.go
+++ solutions.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	print "github.com/01-edu/z01"
@@ -147,14 +148,16 @@ func Atoi(s string) int {
 	return result
 }
 
-func InsertAt(s string, index int, char rune) string {
-	// check the index out of bounds
-	if index < 0 || index >= len(s) {
-		return ""
+func InsertAt(s string, index int, r rune) string {
+	if index < 0 {
+		index = 0
+	} else if index >= len(s) {
+		index = len(s)
 	}
+
 	runes := []rune(s)
-	// insert at a certain index
-	result := append(runes[index:], append([]rune{char}, runes[index:]...)...)
+	result := append(runes[:index], append([]rune{r}, runes[index:]...)...)
+
 	return string(result)
 }
 
@@ -199,4 +202,184 @@ func ReduceInt(a []int, f func(int, int) int) int {
 		result = f(result, a[i])
 	}
 	return result
+}
+
+func Rot13(s string) string {
+	result := []rune{}
+	for _, char := range s {
+		if char >= 'a' && char <= 'z' {
+			result = append(result, 'a'+(char-'a'+(13))%26)
+		} else if char >= 'A' && char <= 'Z' {
+			result = append(result, 'A'+(char-'A'+(13))%26)
+		} else {
+			result = append(result, char)
+		}
+	}
+
+	return string(result)
+}
+
+func AlphaMirror(s string) string {
+	if s == "" {
+		return ""
+	}
+	lowerMap := make(map[rune]rune, 26)
+
+	j := 122
+	for i := 'a'; i < 'z'; i++ {
+		lowerMap[rune(i)] = rune(j)
+		j--
+	}
+
+	upperMap := make(map[rune]rune, 26)
+	j = 90
+	for i := 'A'; i < 'Z'; i++ {
+		upperMap[rune(i)] = rune(j)
+		j--
+	}
+	result := []rune{}
+	for _, char := range s {
+		if IsLower(char) {
+			result = append(result, lowerMap[char])
+		} else if IsUpper(char) {
+			result = append(result, upperMap[char])
+		} else {
+			result = append(result, char)
+		}
+	}
+	return string(result)
+}
+
+func Mirror(r rune) rune {
+	if r >= 'a' && r < 'z' {
+		return 'a' + 'z' - r
+	} else if r >= 'A' && r < 'Z' {
+		return 'A' + 'Z' - r
+	}
+
+	return r
+}
+
+func IsLower(r rune) bool {
+	if r >= 'a' && r <= 'z' {
+		return true
+	}
+	return false
+}
+
+func IsUpper(r rune) bool {
+	if r >= 'A' && r <= 'A' {
+		return true
+	}
+	return false
+}
+
+func AlphaCount(s string) int {
+	alphacount := 0
+
+	for _, char := range s {
+		if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' {
+			alphacount++
+		}
+	}
+	return alphacount
+}
+
+func AlphaPosition(r rune) int {
+	if r >= 'a' && r <= 'z' {
+		return int(r-'a') + 1
+	} else if r >= 'A' && r <= 'Z' {
+		return int(r-'A') + 1
+	}
+	return -1
+}
+
+func Chunk(slice []int, size int) {
+	if len(slice) == 0 {
+		fmt.Println("[]")
+		return
+	}
+	if size == 0 {
+		fmt.Println()
+		return
+	}
+
+	// slice of slices
+	ss := [][]int{}
+	for i := 0; i < len(slice); i += size {
+		end := i + size
+		if end > len(slice) {
+			end = len(slice)
+		}
+		ss = append(ss, slice[i:end])
+	}
+	fmt.Println(ss)
+}
+
+func Compare(a, b string) int {
+	if len(a) == 0 && len(b) == 0 {
+		return 0
+	}
+
+	if a == b {
+		return 0
+	}
+
+	na := len(a)
+	nb := len(b)
+	if na > nb && StartsWith(a, b) {
+		return 1
+	}
+
+	if na > nb && EndsWith(a, b) {
+		return -1
+	}
+
+	return -1
+}
+
+func StartsWith(a, b string) bool {
+	for i := 0; i < len(b); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func EndsWith(a, b string) bool {
+	for i := len(b) - 1; i >= 0; i-- {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func FoldInt(f func(int, int) int, a []int, n int) int {
+	for _, char := range a {
+		n = f(n, char)
+	}
+	return n
+}
+
+
+func FindPrevPrime(){
+
+}
+
+//TrialDivisionAlgorithm
+func TrialDivision(n int) bool{
+
+	//function to check if a number is prime using trial division algorithm
+
+	if n < 2 {
+		return false
+	}
+
+	i := 2
+	k := n**0.5
+	for (i < n){
+		
+	}
 }
